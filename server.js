932549816285941ab2cf3a7e2d867ef3f425561d
PhilV1 <werpub.phil@gmx.de>
2024-05-27 09:57:19 +0200

    refactor: add endpoint to fetch all Todos from the database

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './database/connectDB.js'
+import Todo from './models/todoModel.js'
+
 dotenv.config()
 
 const app = express()
@@ -9,6 +11,7 @@ app.use(express.json())
 
 const port = process.env.PORT || 5060
 
+// get all Todos from DB
 app.get('/todos', async (req, res) => {
   try {
     const todos = await Todo.find()
